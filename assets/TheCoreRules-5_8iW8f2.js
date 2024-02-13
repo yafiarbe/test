6406a1b190926be112becb0f5537372fbe2f0e95
YAUHENI FIARBEI <128324742+yafiarbe@users.noreply.github.com>
2024-02-13 19:17:40 +0100

    Add files via upload

---
+++ assets/TheCoreRules-5_8iW8f2.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-ClQ3aOjb.js";const n={};function r(t,s){return o(),c("section",null,"Основные правила")}const a=e(n,[["render",r]]);export{a as default};
---
+++ assets/TheHome-CtAGwP2k.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-ClQ3aOjb.js";const s={},t={class:""};function n(r,_){return o(),c("section",t,"Главная страница")}const f=e(s,[["render",n]]);export{f as default};
